7d8cbcb5b6203eecc9f8a2eee4278c0b2e67f29b
tombush0 <tommzach@gmail.com>
2019-12-21 23:24:24 +0100

    Finished

--- src/main/Interfaces/IMapElement.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-
-import MapMech.Vector2d;
-
-public interface IMapElement {
-    Vector2d getPosition();
-    String toString();
-}
--- src/main/Interfaces/IPositionChangeObserver.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-import AnimalMech.Animal;
-import MapMech.Vector2d;
-
-public interface IPositionChangeObserver {
-    void positionChanged(Vector2d oldPosition, Vector2d newPosition, Animal animal);
-
-}
